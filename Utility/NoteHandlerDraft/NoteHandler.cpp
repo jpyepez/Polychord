bc260b3b1e9dabebfe966484c6a769becc843e5a
JP Robot Laptop <jpyepezimc@gmail.com>
2020-08-05 17:57:49 +1200

    added utility and note handler

---
+++ Utility/NoteHandlerDraft/NoteHandler.cpp
@@ -0,0 +1,43 @@
+
+
+/*
+ * NoteHandler.h - Chordophone note handler
+ * Created by JP Yepez, Aug 2020.
+ * Victoria University of Wellington
+*/
+
+#include "Arduino.h"
+#include <iostream>
+#include <algorithm>
+#include "NoteHandler.h"
+
+NoteHandler::NoteHandler(const int *mArray, const int *pArray, int aSize) : arraySize(aSize)
+{
+    midiArray = new int[arraySize];
+    posArray = new int[arraySize];
+    std::copy(mArray, mArray + arraySize, midiArray);
+    std::copy(pArray, pArray + arraySize, posArray);
+}
+
+int NoteHandler::getPos(int idx)
+{
+    return posArray[idx];
+}
+
+int NoteHandler::lookupPos(int midi)
+{
+    int *query;
+    int idx;
+
+    query = std::find(midiArray, midiArray + arraySize, midi);
+
+    if (query != midiArray + arraySize)
+    {
+        idx = std::distance(midiArray, query);
+        return posArray[idx];
+    }
+    else
+    {
+        return -1;
+    }
+}
---
+++ Utility/NoteHandlerDraft/NoteHandlerDraft.ino
@@ -0,0 +1,78 @@
+
+#include "NoteHandler.h"
+#include <Metro.h>
+
+// TODO: callbacks or functions
+
+const int PSIZE = 5;
+int midi[PSIZE] = {60, 61, 62, 63, 64};
+int pos[PSIZE] = {1000, 1100, 1200, 1300, 1400};
+NoteHandler noteHandler(midi, pos, PSIZE);
+
+Metro metro = Metro(1000);
+
+// Serial input
+const byte numChars = 32;
+char receivedChars[numChars];
+bool newData = false;
+
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println("Note Handler Start");
+    Serial.println("Input integers:");
+}
+
+void loop()
+{
+    serialRead();
+    parseAndPrintData();
+    //if (metro.check() == 1)
+    //{
+    //    Serial.println(noteHandler.lookupPos(60));
+    //}
+}
+
+void serialRead()
+{
+    static byte ndx = 0;
+    char endMarker = '\n';
+    char rc;
+
+    while (Serial.available() > 0 && newData == false)
+    {
+        rc = Serial.read();
+
+        if (rc != endMarker)
+        {
+            receivedChars[ndx] = rc;
+            ndx++;
+            if (ndx >= numChars)
+            {
+                ndx = numChars - 1;
+            }
+        }
+        else
+        {
+            receivedChars[ndx] = '\0';
+            ndx = 0;
+            newData = true;
+        }
+    }
+}
+
+void parseAndPrintData()
+{
+    if (newData == true)
+    {
+        // Monitor serial
+        Serial.print("Received: ");
+        Serial.println(receivedChars);
+
+        // Note Handler Lookup
+        int val = atoi(receivedChars);
+        Serial.println(noteHandler.lookupPos(val));
+
+        newData = false;
+    }
+}
