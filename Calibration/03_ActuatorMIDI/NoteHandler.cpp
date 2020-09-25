bf7ab52e1c6d4df3ebe04b8ef36cffb6c77690c9
JP Robot Laptop <jpyepezimc@gmail.com>
2020-09-25 17:07:04 +1200

    added tremolo mode

---
+++ Calibration/03_ActuatorMIDI/NoteHandler.cpp
@@ -0,0 +1,54 @@
+
+
+/*
+ * NoteHandler.h - Chordophone note handler
+ * Created by JP Yepez, Aug 2020.
+ * Victoria University of Wellington
+*/
+
+#include "Arduino.h"
+#include <iostream>
+#include <algorithm>
+#include "NoteHandler.h"
+
+NoteHandler::NoteHandler(const int *mArray, const int *pArray, int aSize) : arraySize(aSize)
+{
+    midiArray = new int[arraySize];
+    posArray = new int[arraySize];
+    std::copy(mArray, mArray + arraySize, midiArray);
+    std::copy(pArray, pArray + arraySize, posArray);
+}
+
+int NoteHandler::getPos(int idx)
+{
+    return posArray[idx];
+}
+
+// input midi note, return position at same index
+int NoteHandler::lookupPos(int midiNote)
+{
+    int *query;
+    int idx;
+
+    query = std::find(midiArray, midiArray + arraySize, midiNote);
+
+    if (query != midiArray + arraySize)
+    {
+        idx = std::distance(midiArray, query);
+        return posArray[idx];
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+typedef void (*callback)(int);
+
+// look up position and apply callback
+void NoteHandler::applyPos(int midiNote, callback cb)
+{
+    int pos = lookupPos(midiNote);
+    if (pos != -1)
+        cb(pos);
+}
---
+++ Calibration/03_ActuatorMIDI/NoteHandler.h
@@ -0,0 +1,30 @@
+
+/*
+ * NoteHandler.h - Chordophone note handler
+ * Created by JP Yepez, Aug 2020.
+ * Victoria University of Wellington
+*/
+
+#ifndef NoteHandler_h
+#define NoteHandler_h
+
+#include "Arduino.h"
+
+class NoteHandler
+{
+private:
+    int *posArray;
+    int *midiArray;
+    int arraySize;
+
+    typedef void (*callback)(int);
+
+public:
+    NoteHandler(const int *, const int *, int);
+
+    int getPos(int);
+    int lookupPos(int);
+    void applyPos(int, callback);
+};
+
+#endif
